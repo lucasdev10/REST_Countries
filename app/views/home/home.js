c2493b354efc2ccb7a92f684112ba0ac6455ad95
lucasdev10 <lucas.adsjr@gmail.com>
2021-08-09 19:01:33 -0300

    Creating filter function

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,7 +1,15 @@
 const countryItems = document.getElementById('countries-container');
+var allCountries = [];
+var allCountriesBackup = [];
+var regionSelected = 'africa';
 
 const findAllCountries = async (region) => {
-   const allCountries = await getCountriesByRegion(region);
+   allCountries = await getCountriesByRegion(region);
+   allCountriesBackup = allCountries;
+   handlerHTML();
+};
+
+handlerHTML = () => {
    allCountries.forEach(country => {
       const countryItem = document.createElement('a');
       countryItem.classList.add('country-item');
@@ -18,7 +26,23 @@ const findAllCountries = async (region) => {
 
 findAllCountries('africa');
 
-chooseRegion = (region) => { 
+chooseRegion = (region) => {
    countryItems.innerHTML = '';
-   findAllCountries(region); 
+   regionSelected = region;
+   findAllCountries(region);
 }
+
+filterCountries = (search) => {
+   countryItems.innerHTML = '';
+   if (search.trim()) {
+      allCountries = allCountriesBackup.filter(el => el.name.toLowerCase().includes(search.toLowerCase()));
+      handlerHTML();
+   } else {
+      resetValues();
+   }
+};
+
+resetValues = () => {
+   allCountries = allCountriesBackup;
+   handlerHTML();
+};
